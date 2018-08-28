a1ca9daf3bd8131a160f7e4a4b64e16aec9b77cd
Dania Maciel <daniaemily@gmail.com>
2018-08-28 13:23:11 -0400

    added border bottom animation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav-header">
     <ul>
-      <button
+      <button class="animatedBorder"
         @click="handleClick(categoryId)"
         v-for="(articleCategory, categoryId) in articleCategories">
         <p>{{ articleCategory.label }}</p>
@@ -39,6 +39,7 @@
   display: flex;
   justify-content: space-evenly;
   padding-left: 0px;
+/*  background-color: yellow;*/
 }
 
 .category-nav button {
@@ -49,6 +50,7 @@
   padding-bottom: 3px;
   text-align: center;
   border: none;
+/*  margin-top: -24px;*/
 }
 
 .category-nav button:hover {
@@ -61,5 +63,25 @@
   position: sticky;
   top: -1px;
 }
+
+.animatedBorder {
+  position: relative;
+}
+
+.animatedBorder:after {
+  content: '';
+  position: absolute;
+  border-bottom: 5px solid #87E5DA;;
+  bottom: 0;
+  left: 0;
+  right: 100%;
+  transition: all 500ms;
+}
+
+.animatedBorder:hover:after {
+  right: 0;
+}
+
+
 
 </style>
